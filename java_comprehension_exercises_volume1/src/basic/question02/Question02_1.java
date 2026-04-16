fabd95b3fbc6062a0a4278c50c4aa457009c6af0
r-hashiguchi <r-hashiguchi@CD26308L.arksystems.co.jp>
2026-04-16 11:07:16 +0900

    question02

--- java_comprehension_exercises_volume1/src/basic/question02/Question02_1.java
+++ java_comprehension_exercises_volume1/src/basic/question02/Question02_1.java
@@ -1,6 +1,8 @@
 package basic.question02;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * 第5章 入出力
@@ -11,6 +13,18 @@ public class Question02_1 {
 	public static void main(String[] args) throws IOException {
 
 		// TODO:ここに実装
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.println("名前を入力してください。");
+		System.out.print(">");
+		String name = reader.readLine();
+
+		System.out.println("\n好きなスポーツを入力してください。");
+		System.out.print(">");
+		String sport = reader.readLine();
+
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("好きなスポーツは" + sport + "です。");
 
 	}
 }
--- java_comprehension_exercises_volume1/src/basic/question02/Question02_2.java
+++ java_comprehension_exercises_volume1/src/basic/question02/Question02_2.java
@@ -1,6 +1,8 @@
 package basic.question02;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * 第5章 入出力
@@ -11,6 +13,18 @@ public class Question02_2 {
 	public static void main(String[] args) throws IOException {
 
 		// TODO:ここに実装
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.println("商品の名前と値段を入力してください。");
+		System.out.print("商品名>");
+		String itemName = reader.readLine();
+
+		System.out.print("値段>");
+		String Price = reader.readLine();
+		int itemPrice = Integer.parseInt(Price);
+
+		System.out.println("商品の名前は" + itemName + "です。");
+		System.out.println("商品の値段は" + itemPrice + "円です。");
 
 	}
 }
